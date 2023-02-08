97cc41c6231f8b10db534f52c7171dde403a5ace
argray04 <abigail86035@students.sbcsc.k12.in.us>
2023-02-08 08:38:59 -0500

    wrote some new pseudocode

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+//Write a function that randomly returns rock, paper, or scissors
+
+getComputerChoice(); {
+    const choices = ["rock", "paper", "scissors"];
+    get(index)  
+}
+let ComputerChoice = getComputerChoice();
+
+
+//write a function that plays a single round of Rock, Paper, Scissors. 
+//Should take playerSelection and computer Selection, then return
+// You win or lose!
+
+
+//pseudocode
+
+//ComputerChoice function
+        //Calls random string "rock", "paper", or "scissors" from a list
+        //That choice goes into a new variable for the next function
+//RockPaperScissors function
+        //Ask for case-sensitive player choice
+        //puts that into the player choice variable
+        //must have conditions for all the outcomes of rps
+
+        //r*r = draw
+        //p*p = draw
+        //s*s = draw
+        //r*p = p wins
+        //p*s = s wins
+        //r*s = r wins
